e59b82fd14c68011d5cb1222a50d7a3a07ed528f
yangsungho <postjoyboy@naver.com>
2023-05-20 19:57:36 +0900

    c만들었음

--- asgeg.js
+++ asgeg.js
@@ -1,1 +1,13 @@
-asdfasdfsf
+import React, { useState, useEffect } from 'react';
+import Timer from './Timer';
+
+function App(){
+  const [showTimer, setShowTimer] = useState(false);
+
+  return (
+    <div>
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+    </div>
+  );
+}
